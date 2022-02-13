51a05aa5ebd3cbd97c3cb516aa6429a55cb328f5
InsonusK <insonus.k@gmail.com>
2022-02-13 17:12:39 +0300

    add logs and increase version for finam-export loading update

--- src/module/quote_loader.py
+++ src/module/quote_loader.py
@@ -1,13 +1,18 @@
 from datetime import datetime
 import pandas as pd
-from finam import Exporter, Market,Timeframe
+from finam import Exporter, Market,Timeframe, FinamParsingError
 import logging
 
 def load_quote(stock_idx:int, market: Market, start_date:datetime, end_date:datetime, timeframe:Timeframe)->pd.DataFrame:
     logger = logging.getLogger("load_quote")
     logger.info(f'Load quote for: stock_idx = {stock_idx}, market = {market}, date_from = {start_date}, date_till = {end_date}, timframe = {timeframe}')
     
-    ret:pd.DataFrame = Exporter().download(stock_idx,market,start_date,end_date,timeframe )
+    try:
+        ret:pd.DataFrame = Exporter().download(stock_idx,market,start_date,end_date,timeframe )
+    except FinamParsingError as ex:
+        logger.warning(f"Get error while parsing: {ex}")    
+        raise ex
+    
     if len(ret.index) == 0:  
         return ret
     ret.rename(columns={"<CLOSE>": "Close", "<OPEN>": "Open", "<HIGH>": "High", "<LOW>": "Low", "<VOL>": "Volume"},
@@ -24,7 +29,4 @@ def load_quote(stock_idx:int, market: Market, start_date:datetime, end_date:date
     ret = ret.round(9)
     ret["DT"] = ret["DT"].map(lambda el: el.isoformat())
     return ret
-
-
-    
-    
+  
